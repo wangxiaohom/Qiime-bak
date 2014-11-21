081c4f85009aeb978ba6e8f9a28c6c1b054ad4b4
wxhyihuan <wxh244295043@live.cn>
2014-11-21 11:05:59 +0800

    ds
    dsf   sdf

---
+++ AmpliconNoiseV1.27/Scripts/Parse.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+my $tag = shift(@ARGV);
+my $newLength = shift(@ARGV);
+
+$tag =~ /G*(.*)/;
+$tag2 = &translateIUPAC($1);
+
+#print "$tag2\n";
+
+while($line = <STDIN>){
+    chomp($line);
+
+    if($line =~ />(.*)/){
+	print "$line\n";
+    }
+    else{
+	$line =~ /$tag2(.*)/; 	
+	$parse = $1;
+	$truncate = substr($parse, 0, $newLength);
+
+	print "$truncate\n";
+    }
+}
+
+sub translateIUPAC()
+{
+  my ($seq) = @_;
+
+  $seq=~s/W/\[AT\]/g;
+  $seq=~s/B/\[CGT\]/g;
+  $seq=~s/S/\[GC\]/g;  
+  $seq=~s/N/\[ACTG\]/g;
+  $seq=~s/Y/\[CT\]/g;
+  $seq=~s/R/\[AG\]/g;
+  $seq=~s/D/\[AGT\]/g;
+  $seq=~s/H/\[ACT\]/g;
+  $seq=~s/M/\[AC\]/g;
+  $seq=~s/K/\[GT\]/g;
+  $seq=~s/V/\[ACG\]/g;
+  return $seq;
+}
+
+
